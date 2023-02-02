3fd232612f38c1354388eefd502a1485b7446e6e
Le Manour David <dlemanour@greenflex.com>
2023-02-02 18:14:39 +0100

    hackaton

--- api/src/Entity/Sale.php
+++ api/src/Entity/Sale.php
@@ -2,31 +2,81 @@
 
 namespace App\Entity;
 
+
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\SaleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
 
 #[ORM\Entity(repositoryClass: SaleRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Put(),
+        new Post(),
+        new Get(),
+        new Patch(),
+        new Delete(),
+        new GetCollection(
+            normalizationContext: ["groups" => ["sale:read", "sale:collection:get"]],
+        ),
+    ],
+    routePrefix: "/api",
+    normalizationContext: ["groups" => ["sale:read", ]],
+    denormalizationContext: ["groups" => ["sale:write", ]],
+    mercure: true,
+)]
+
 class Sale
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(["sale:read"])]
+
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(["sale:read", "sale:write", "bid:item:get", 'bid:collection:get'])]
     private ?string $model = null;
 
     #[ORM\Column(type: Types::SMALLINT)]
+    #[Groups(["sale:read", "sale:write", "bid:item:get", 'bid:collection:get'])]
     private ?int $unit = null;
 
     #[ORM\Column]
+    #[Groups(["sale:read", "sale:write", "bid:item:get", 'bid:collection:get'])]
     private ?float $baseUnitPrice = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(["sale:read", "sale:write", "bid:item:get", 'bid:collection:get'])]
     private ?string $status = null;
+
+    #[ORM\JoinColumn(nullable: false)]
+    #[ORM\ManyToOne(cascade: ["persist"], inversedBy: 'sales')]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups(["user:read", "user:write","sale:read","sale:write"])]
+    #[Assert\Valid]
+    private ?User $seller = null;
+
+    #[ORM\OneToMany(mappedBy: 'sale', targetEntity: Bid::class, cascade: ["persist"], orphanRemoval:true)]
+    #[Groups(["sale:read"])]
+    #[Assert\Valid]
+    private Collection $bids;
+
+    public function __construct()
+    {
+        $this->bids = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -77,6 +127,48 @@ class Sale
     public function setStatus(string $status): self
     {
         $this->status = $status;
+
+        return $this;
+    }
+
+    public function getSeller(): ?User
+    {
+        return $this->seller;
+    }
+
+    public function setSeller(?User $seller): self
+    {
+        $this->seller = $seller;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Bid>
+     */
+    public function getBids(): Collection
+    {
+        return $this->bids;
+    }
+
+    public function addBid(Bid $bid): self
+    {
+        if (!$this->bids->contains($bid)) {
+            $this->bids->add($bid);
+            $bid->setSale($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBid(Bid $bid): self
+    {
+        if ($this->bids->removeElement($bid)) {
+            // set the owning side to null (unless already changed)
+            if ($bid->getSale() === $this) {
+                $bid->setSale(null);
+            }
+        }
 
         return $this;
     }
--- api/src/State/RegistrationStateProcessor.php
+++ api/src/State/RegistrationStateProcessor.php
@@ -26,6 +26,6 @@ class RegistrationStateProcessor implements ProcessorInterface
         }
 
         $this->persistProcessor->process($data, $operation, $uriVariables, $context);
-        $this->mailVerifyEmail->send($data);
+        //$this->mailVerifyEmail->send($data);
     }
 }
